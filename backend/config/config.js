b40eec1297e7d660e6d1cecd2de7001e42be1674
vidita <vidita@Viditas-MacBook-Pro.local>
2019-11-12 11:20:07 -0800

    Fixed the kafka url to not include protocol and fixed issue with exporting module

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,7 +1,7 @@
  const config = {
   secret: "cmpe273_kafka_passport_mongo",
   frontendURI: "http://localhost:3000",
-  kafkaURI: "http://localhost:2181"
+   kafkaURI: "localhost:2181"
 };
 
 module.exports = config;
