875e9ae365c24f4c68aacf33dbe9c21de292430f
Manovikas Ramaswamy <manovikas.ramaswamy@sjsu.edu>
2019-11-15 14:10:17 -0800

    Updated RDS Instance details.

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -4,8 +4,8 @@ const config = {
    frontendURI: "http://localhost:3000",
    kafkaURI: "localhost:2181",
    mysqlUser: "root",
-   mysqlPassword: "cmpetwitter",
-   mysqlHost: "twitter.cxwydwkipclw.us-east-1.rds.amazonaws.com",
+   mysqlPassword: "cmpepassword",
+   mysqlHost: "cmpedatabase.cxwydwkipclw.us-east-1.rds.amazonaws.com",
    mysqlDatabase: "twitter"
 };
 
