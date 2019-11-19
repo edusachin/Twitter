dfe77c303725ac54d81e460462b8c9140645fbc7
Vidita <viditavijaykumar.daga@sjsu.edu>
2019-11-19 11:27:02 -0800

    Removed unwanted code

--- kafka-backend/kafka/connection.js
+++ kafka-backend/kafka/connection.js
@@ -3,24 +3,16 @@ var kafka = require("kafka-node");
 const { kafkaURI } = require("../utils/config");
 
 function ConnectionProvider() {
-
   this.getConsumer = function (topic_name) {
     var options = {
-      // connect directly to kafka broker (instantiates a KafkaClient)
       kafkaHost: kafkaURI,
       groupId: topic_name,
       autoCommit: true,
       autoCommitIntervalMs: 5000,
       sessionTimeout: 15000,
       fetchMaxBytes: 10 * 1024 * 1024, // 10 MB
-      // An array of partition assignment protocols ordered by preference. 'roundrobin' or 'range' string for
-      // built ins (see below to pass in custom assignment protocol)
       protocol: ['roundrobin'],
-      // Offsets to use for new groups other options could be 'earliest' or 'none'
-      // (none will emit an error if no offsets were saved) equivalent to Java client's auto.offset.reset
       fromOffset: 'latest',
-      // how to recover from OutOfRangeOffset error (where save offset is past server retention)
-      // accepts same value as fromOffset
       outOfRangeOffset: 'earliest'
     };
     var consumer = new kafka.ConsumerGroup(options, topic_name);
