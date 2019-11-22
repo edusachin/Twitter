40c0e99368cd7db4e377c1d60a5a3af137995a52
Shivanijain21 <shivani.jain@sjsu.edu>
2019-11-22 02:30:15 -0800

    adding tweets card and post tweet

--- frontend/src/components/analytics-module/analytics.jsx
+++ frontend/src/components/analytics-module/analytics.jsx
@@ -3,9 +3,10 @@ import React, { Component } from 'react';
 class Analytics extends Component {
     state = {}
     render() {
-        return (<div className="row analytics-section">
-            <h2 className="content-title col-sm-12">Analytics</h2>
-        </div>);
+        return (
+            <div className="row analytics-section">
+                <h2 className="content-title col-sm-12">Analytics</h2>
+            </div>);
     }
 }
 
