07eb99b1b8e58c2be3c253fb973d6325b6ca89be
Shivanijain21 <shivani.jain@sjsu.edu>
2019-11-21 23:58:19 -0800

    adding styles in sidebar

--- frontend/src/components/sidebar/sidebar.jsx
+++ frontend/src/components/sidebar/sidebar.jsx
@@ -6,29 +6,31 @@ class Sidebar extends Component {
     state = {}
     render() {
         return (
-            <div>
-                <img src={twitter_icon} className="twitter_icon" />
-                <ul >
-                    <li >
-                        <NavLink className="nav-link" to="/home" exact={true} >Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink className="nav-link" to="/messages" exact={true} >Messages</NavLink>
-                    </li>
-                    <li>
-                        <NavLink className="nav-link" to="/bookmarks" exact={true}>Bookmarks</NavLink>
-                    </li>
-                    <li>
-                        <NavLink className="nav-link" to="/lists" exact={true}>Lists</NavLink>
-                    </li>
-                    <li>
-                        <NavLink className="nav-link" to="/profile" exact={true}>Profile</NavLink>
-                    </li>
-                    <li>
-                        <NavLink className="nav-link" to="/analytics" exact={true}>Analytics</NavLink>
-                    </li>
-                </ul>
-            </div>
+            <div className="row sidebar">
+                <div className="col-sm-12">
+                    <div className="col-sm-12 py-3">
+                        <img src={twitter_icon} className="twitter_icon" />
+                    </div>
+                    <div className="col-sm-12 py-3 sidebarItem">
+                        <NavLink className="p-2 pr-4" to="/home" exact={true} >Home</NavLink>
+                    </div>
+                    <div className="col-sm-12 py-3 sidebarItem">
+                        <NavLink className="p-2 pr-4" to="/messages" exact={true} >Messages</NavLink>
+                    </div>
+                    <div className="col-sm-12 py-3 sidebarItem">
+                        <NavLink className="p-2 pr-4" to="/bookmarks" exact={true}>Bookmarks</NavLink>
+                    </div>
+                    <div className="col-sm-12 py-3 sidebarItem">
+                        <NavLink className="p-2 pr-4" to="/lists" exact={true}>Lists</NavLink>
+                    </div>
+                    <div className="col-sm-12 py-3 sidebarItem">
+                        <NavLink className="p-2 pr-4" to="/profile" exact={true}>Profile</NavLink>
+                    </div>
+                    <div className="col-sm-12 py-3 sidebarItem">
+                        <NavLink className="p-2 pr-4" to="/analytics" exact={true}>Analytics</NavLink>
+                    </div>
+                </div>
+            </div >
         );
     }
 }
