11d0b148fb87194fe6ec34fe835d1abac60881ed
Shivanijain21 <shivani.jain@sjsu.edu>
2019-11-21 23:02:32 -0800

    twitter_icon

--- frontend/src/components/sidebar/sidebar.jsx
+++ frontend/src/components/sidebar/sidebar.jsx
@@ -1,31 +1,34 @@
 import React, { Component } from 'react';
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import twitter_icon from "../../twitter_icon.png";
 
 class Sidebar extends Component {
     state = {}
     render() {
         return (
-
-            <ul >
-                <li >
-                    <NavLink className="nav-link" to="/home" exact={true} >Home</NavLink>
-                </li>
-                <li>
-                    <NavLink className="nav-link" to="/messages" exact={true} >Messages</NavLink>
-                </li>
-                <li>
-                    <NavLink className="nav-link" to="/bookmarks" exact={true}>Bookmarks</NavLink>
-                </li>
-                <li>
-                    <NavLink className="nav-link" to="/lists" exact={true}>Lists</NavLink>
-                </li>
-                <li>
-                    <NavLink className="nav-link" to="/profile" exact={true}>Profile</NavLink>
-                </li>
-                <li>
-                    <NavLink className="nav-link" to="/analytics" exact={true}>Analytics</NavLink>
-                </li>
-            </ul>
+            <div>
+                <img src={twitter_icon} className="twitter_icon" />
+                <ul >
+                    <li >
+                        <NavLink className="nav-link" to="/home" exact={true} >Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className="nav-link" to="/messages" exact={true} >Messages</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className="nav-link" to="/bookmarks" exact={true}>Bookmarks</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className="nav-link" to="/lists" exact={true}>Lists</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className="nav-link" to="/profile" exact={true}>Profile</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className="nav-link" to="/analytics" exact={true}>Analytics</NavLink>
+                    </li>
+                </ul>
+            </div>
         );
     }
 }
