a027b84e61e34a91f8bc7a42eda332ca6f4c4ac5
Vidita <viditavijaykumar.daga@sjsu.edu>
2019-11-26 13:18:01 -0800

    Removed AWS Access key from config file since its public

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -8,8 +8,9 @@ const config = {
    mysqlHost: "cmpedatabase.cxwydwkipclw.us-east-1.rds.amazonaws.com",
    mysqlDatabase: "twitter",
    awsBucket: "cmpe273twitter",
-   awsAccessKey: "AKIAISPOF2TXR2WO3WYA",
-   awsSecretAccessKey: "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",
+   // Keys can't be added here because AWS categorizes this as vulnerability.
+   awsAccessKey: "",
+   awsSecretAccessKey: "",
    awsPermission: "public-read"
 };
 
