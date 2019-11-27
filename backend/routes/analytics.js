ac07606a66e19aa52728cc64b4b752e7c3adcc60
Vidita <viditavijaykumar.daga@sjsu.edu>
2019-11-26 19:09:15 -0800

    Implemented API to get top 10 liked tweets

--- backend/routes/analytics.js
+++ backend/routes/analytics.js
@@ -20,4 +20,22 @@ router.get("/topViewedTweets", async (req, res) => {
     });
 });
 
+/**
+ * To get Top 10 tweets by likes
+ */
+router.get("/topLikedTweets", async (req, res) => {
+    let msg = {};
+    msg.count = 10,
+        msg.route = "get_top_liked_tweets";
+    kafka.make_request("analytics", msg, function (err, results) {
+        if (err) {
+            console.log("-------error: tweet:get/:id---------");
+            res.status(err.status).send(err.data);
+        }
+        else {
+            res.status(results.status).send(results.data);
+        }
+    });
+});
+
 module.exports = router;
