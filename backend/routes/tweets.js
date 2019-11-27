2a8731004e70fdc6e3920590a158b8cd6fb1856c
Manovikas Ramaswamy <manovikas.ramaswamy@sjsu.edu>
2019-11-26 17:49:24 -0800

    Created a new topic for tweet_actions.

--- backend/routes/tweets.js
+++ backend/routes/tweets.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const kafka = require("../kafka/client");
-const { validateTweet, validateLikes, validateReplies } = require("../validations/tweetValidations");
+const { validateTweet } = require("../validations/tweetValidations");
 const { STATUS_CODE } = require("../utils/constants");
 const uploadFileToS3 = require('../utils/awsImageUpload');
 const multer = require('multer');
@@ -82,6 +82,27 @@ router.get("/tweet/:tweet_id", async (req, res) => {
 });
 
 /**
+ * To get all the likes done by a user
+ */
+router.get("/liked/:user_id", async (req, res) => {
+    let msg = {
+        user_id: req.params.user_id,
+        route: "get_user_liked_tweets"
+    }
+
+    kafka.make_request("tweets", msg, function (err, results) {
+        if (err) {
+            console.log("-------error: tweet:get_user_likes/:id---------");
+            res.status(err.status).send(err.data);
+        }
+        else {
+            res.status(results.status).send(results.data);
+        }
+    });
+
+});
+
+/**
  * Post a tweet
  * @param req: user_id,tweet_text
  */
@@ -116,28 +137,6 @@ router.post("/", upload.any(), async (req, res) => {
 });
 
 /**
- * To retweet a tweet
- * @param req:user_id, tweet_id
- */
-router.post("/retweet", async (req, res) => {
-    const { error } = false;
-    if (error) {
-        console.log("-------error: tweet:post/retweet/---------");
-        res.status(STATUS_CODE.BAD_REQUEST).send(error.details[0].message);
-    }
-    let msg = req.body;
-    msg.route = "post_retweet";
-    kafka.make_request("tweets", msg, function (err, results) {
-        if (err) {
-            res.status(err.status).send(err.data);
-        }
-        else {
-            res.status(results.status).send(results.data);
-        }
-    });
-});
-
-/**
  * To delete a tweet
  * @param req: user_id, tweet_id
  */
@@ -157,92 +156,6 @@ router.post("/delete", async (req, res) => {
             res.status(results.status).send(results.data);
         }
     });
-});
-
-/**
- * To post a reply on a tweet
- * @param req:  user_id,tweet_id
- */
-router.post("/likes", async (req, res) => {
-    const { error } = validateLikes(req.body);
-    if (error) {
-        res.status(STATUS_CODE.BAD_REQUEST).send(error.details[0].message);
-    }
-    let msg = req.body;
-    msg.route = "post_likes";
-    kafka.make_request("tweets", msg, function (err, results) {
-        console.log('Inside backend');
-        console.log(results);
-        if (err) {
-            res.status(err.status).send(err.data);
-        }
-        else {
-            res.status(results.status).send(results.data);
-        }
-    });
-});
-
-/**
- * To post a reply on a tweet
- * @param req: user_id, tweet_id, reply_text
- */
-router.post("/replies", async (req, res) => {
-    const { error } = validateReplies(req.body);
-    if (error) {
-        res.status(STATUS_CODE.BAD_REQUEST).send(error.details[0].message);
-    }
-    let msg = req.body;
-    msg.route = "post_replies";
-    kafka.make_request("tweets", msg, function (err, results) {
-        if (err) {
-            res.status(err.status).send(err.data);
-        }
-        else {
-            res.status(results.status).send(results.data);
-        }
-    });
-});
-
-/**
- * To get all the replies posted by a user
- */
-router.get("/:tweet_id/replies", async (req, res) => {
-    let msg = {
-        tweet_id: req.params.tweet_id,
-        route: "get_tweet_replies"
-    }
-
-    kafka.make_request("tweets", msg, function (err, results) {
-        if (err) {
-            console.log("-------error: tweet:get_replies/:id---------");
-            res.status(err.status).send(err.data);
-        }
-        else {
-            res.status(results.status).send(results.data);
-        }
-    });
-
-});
-
-/**
- * To get all the likes done by a user
- */
-router.get("/:user_id/liked", async (req, res) => {
-    let msg = {
-        user_id: req.params.user_id,
-        route: "get_user_likes"
-    }
-
-    kafka.make_request("tweets", msg, function (err, results) {
-        if (err) {
-            console.log("-------error: tweet:get_user_likes/:id---------");
-            res.status(err.status).send(err.data);
-        }
-        else {
-            res.status(results.status).send(results.data);
-        }
-    });
-
 });
 
 module.exports = router;
