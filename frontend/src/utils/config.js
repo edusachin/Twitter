66200af05ee45c8870f7a0a4418ef9e648d5bf1e
Vidita <viditavijaykumar.daga@sjsu.edu>
2019-12-02 21:40:13 -0800

    Made changes to frontend and backend urls

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    backendURI: "backend-lb2-491005233.us-east-1.elb.amazonaws.com:3001",
+    //backendURI: "3.86.60.175:3001"
+    backendURI: "http://backend-lb2-491005233.us-east-1.elb.amazonaws.com:3001",
 };
 
 // const backendURI = "http://34.205.147.232:3001";
